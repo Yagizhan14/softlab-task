092d65b08f4c43b0008b1c6b0133ad4619e3d372
Yagizhan Avci <ygzhn14@gmail.com>
2019-10-24 16:30:01 +0300

    final commit

--- src/App.js
+++ src/App.js
@@ -2,14 +2,21 @@ import React from "react";
 import "./App.scss";
 import Leftmenu from "./components/Leftmenu";
 import Venues from "./components/Venues";
+import { connect } from "react-redux";
 
-const App = () => {
+const App = ({ venues }) => {
   return (
     <div className="App">
       <Leftmenu />
-      <Venues />
+      <Venues venues={venues} />
     </div>
   );
 };
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    venues: state.venues,
+  };
+};
+
+export default connect(mapStateToProps)(App);
--- src/components/Venues/index.jsx
+++ src/components/Venues/index.jsx
@@ -1,38 +1,117 @@
 import React from "react";
 import "./Venues.scss";
-import { Container, Row } from "react-bootstrap";
-import { connect } from "react-redux";
+import { Container, Row, Col } from "react-bootstrap";
 
 const Venues = ({ venues }) => {
-  console.log(venues);
+  const { venues: venuesList, selectedCategory } = venues;
+  console.log(venuesList);
+  console.log(selectedCategory);
 
   let welcomeScreen = (
-    <Container>
-      <div className="welcome-screen">
-        <h1 className="welcome-screen-header">
-          <span className="brand-name">Valletta Mekanları</span>'na Hoşgeldiniz
-        </h1>
-        <p className="welcome-screen-p">
-          Bu uygulama Malta'nın başkenti olan Valletta şehrindeki önemli buluşma
-          noktalarını içerir. Sol tarafta gördüğünüz kategorilerden herhangi
-          birini seçip buluşma noktalarını görebilirsiniz.
-        </p>
-      </div>
-    </Container>
+    <div className="Venues">
+      <Container>
+        <div className="welcome-screen">
+          <h1 className="welcome-screen-header">
+            <span className="brand-name">Valletta Mekanları</span>'na
+            Hoşgeldiniz
+          </h1>
+          <p className="welcome-screen-p">
+            Bu uygulama Malta'nın başkenti olan Valletta şehrindeki önemli
+            buluşma noktalarını içerir. Sol tarafta gördüğünüz kategorilerden
+            herhangi birini seçip buluşma noktalarını görebilirsiniz.
+          </p>
+        </div>
+      </Container>
+    </div>
   );
 
-  let renderVenues = (
-    <Container>
-      <Row></Row>
-    </Container>
+  let venueStyles;
+
+  switch (selectedCategory) {
+    case "sanat-ve-eglence":
+      venueStyles = {
+        backgroundColor: "crimson",
+      };
+      break;
+    case "universite":
+      venueStyles = {
+        backgroundColor: "dodgerblue",
+      };
+      break;
+    case "etkinlik":
+      venueStyles = {
+        backgroundColor: "coral",
+      };
+      break;
+    case "yemek":
+      venueStyles = {
+        backgroundColor: "#d55252",
+      };
+      break;
+    case "gece-hayati":
+      venueStyles = {
+        backgroundColor: "#8f4426",
+      };
+      break;
+    case "acik-alanlar-ve-dinlence":
+      venueStyles = {
+        backgroundColor: "#c400c6",
+      };
+      break;
+    case "profesyonel-ve-diger-yerler":
+      venueStyles = {
+        backgroundColor: "#293462",
+      };
+      break;
+    case "konut":
+      venueStyles = {
+        backgroundColor: "#ec9b3b",
+      };
+      break;
+    case "magaza-ve-hizmet":
+      venueStyles = {
+        backgroundColor: "#f0134d",
+      };
+      break;
+    case "seyahat-ve-tasima":
+      venueStyles = {
+        backgroundColor: "#2d3d28",
+      };
+      break;
+    default:
+      break;
+  }
+
+  let renderVenues = venuesList.length > 0 && (
+    <div className="Venues">
+      <Container>
+        <Row>
+          {venuesList.map((venue, idx) => {
+            return (
+              <Col sm={12} md={6} lg={4} key={idx}>
+                <div className="venue" style={venueStyles}>
+                  <h4 className="venue-name">
+                    {venue.venue.name && venue.venue.name}
+                  </h4>
+                  <p className="venue-address">
+                    {venue.venue.location.address &&
+                      venue.venue.location.address}{" "}
+                    <span>
+                      {venue.venue.location.city && venue.venue.location.city}/
+                      {venue.venue.location.country &&
+                        venue.venue.location.country}
+                    </span>
+                  </p>
+                </div>
+              </Col>
+            );
+          })}
+        </Row>
+      </Container>
+    </div>
   );
-  return <div className="Venues">{welcomeScreen}</div>;
+
+  return venuesList.length > 0 ? renderVenues : welcomeScreen;
 };
 
-const mapStateToProps = state => {
-  return {
-    venues: state.venues,
-  };
-};
-
-export default connect(mapStateToProps)(Venues);
+export default Venues;
